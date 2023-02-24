380ea351076b87511d51e9fc2b3d3ce0cd1ac889
JPAkira <joaopauloribeiromyszka@gmail.com>
2023-02-24 00:47:09 -0300

    trocando para o dominio

--- tasks/src/services/app.services.tsx
+++ tasks/src/services/app.services.tsx
@@ -1,35 +1,35 @@
 import {appSources, appSourcesWithoutLogin} from "./appSources";
 export class AppService {
     public async getTasks(path = ''): Promise<any> {
-        let source = appSources('http://47.88.1.91/backend/api/tasks/')
+        let source = appSources('https://tasks.akiralab.com.br/backend/api/tasks/')
         const response = source.get(path);
         return response;
     }
     public async deleteTask(id: number): Promise<any> {
-        let source = appSources(`http://47.88.1.91/backend/api/tasks/${id}/`)
+        let source = appSources(`https://tasks.akiralab.com.br/backend/api/tasks/${id}/`)
         const response = await source.delete('');
         return response.data;
     }
     public async postTask(object: any): Promise<any> {
-        let source = appSources('http://47.88.1.91/backend/api/tasks/')
+        let source = appSources('https://tasks.akiralab.com.br/backend/api/tasks/')
         const response = await source.post('', object);
         return response.data;
     }
 
     public async postUser(object: any): Promise<any> {
-        let source = appSources('http://47.88.1.91/backend/api/users/')
+        let source = appSources('https://tasks.akiralab.com.br/backend/api/users/')
         const response = await source.post('', object);
         return response.data;
     }
 
     public async postAuth(object: any): Promise<any> {
-        let source = appSourcesWithoutLogin('http://47.88.1.91/backend/api/token/')
+        let source = appSourcesWithoutLogin('https://tasks.akiralab.com.br/backend/api/token/')
         const response = await source.post('', object);
         return response.data;
     }
 
     public async putTask(event: any, id: any): Promise<any> {
-        let source = appSources('http://47.88.1.91/backend/api/tasks/'.concat(id).concat("/"))
+        let source = appSources('https://tasks.akiralab.com.br/backend/api/tasks/'.concat(id).concat("/"))
         const response = await source.put("", event);
         return response.data;
     }
